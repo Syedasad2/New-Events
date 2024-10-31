5ff17e7a2a3fb0c5db341b6d768fa41fc98ac0bd
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2024-10-31 12:42:49 +0500

    new changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1>Hellow</h1>
+    <div className="min-h-screen">
+    <h1 className="font-bold text-3xl p-20">Find Your Friend</h1>
+    <Link href="/admin">
+    <button>Admin</button></Link>
+     
     </div>
   );
 }

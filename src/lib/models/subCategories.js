46c1e6347529b59cefd45ddc13f51621f14d3493
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2024-11-08 11:10:54 +0500

    new changes

--- src/lib/models/subCategories.js
+++ src/lib/models/subCategories.js
@@ -6,8 +6,8 @@ const subCategoriesSchema = new Schema({
   title: { type: String, required: true },
   description: String,
   thumbnail: { type: String, required: true },
-  category: { type: mongoose.Types.ObjectId, ref: "Categories" },
-  required: true,
+  category: { type: mongoose.Types.ObjectId, ref: "Categories" , required: true,},
+ 
 });
 
 export const subCategoriesmodal =
